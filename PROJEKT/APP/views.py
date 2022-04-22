39754448866ad7042e19d4376169aa1b3e849d28
Polgár Levente <etnevelraglop@gmail.com>
2022-04-22 21:39:22 +0200

    hrd

--- PROJEKT/APP/views.py
+++ PROJEKT/APP/views.py
@@ -1,6 +1,16 @@
+from multiprocessing import context
 from django.shortcuts import render
+
+# Create your views here.
 
 def VIEW(request):
 	return render(request, "APP/EZ.html", {})
 
-# Create your views here.
+
+def index(request):
+	template = "index.html"
+	context = {}
+	return render(request, template, context)
+
+
+
